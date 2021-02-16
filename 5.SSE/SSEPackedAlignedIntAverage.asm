9b603a14be1fd7ff666944ee93c6b83917fcf2b1
KDahir247 <k.dahir243@hotmail.com>
2021-02-15 23:06:41 -0600

    Create SSEPackedAlignedIntAverage.asm
    completed a basic SSE hello world exercise where it get the average of each element of the array and return it to result.

---
+++ 5.SSE/SSEPackedAlignedIntAverage.asm
@@ -0,0 +1,52 @@
+.model flat, c
+;Simple Streaming SIMD Extensions example note that variables are aligned
+;Implementation for this Assembly File
+;extern "C" void SsePackedInt_Average(const Xmm * a, const Xmm* b, Xmm* result);
+
+;c++ file below commented
+.code
+SsePackedInt_Average proc
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp + 8]
+	mov ecx, [ebp + 12]
+	mov edx, [ebp + 16]
+
+	movdqa xmm0, [eax]
+	movdqa xmm1, [ecx]
+
+	pavgw xmm0, xmm1
+
+	movdqa [edx], xmm0
+	
+	pop ebp
+	ret
+SsePackedInt_Average endp
+end
+
+
+;#include <iostream>
+;
+;struct Xmm
+;{
+;	__int16 i16[8];
+;};
+;
+;extern "C" void SsePackedInt_Average(const Xmm * a, const Xmm* b, Xmm* result);
+;
+;int main()
+;{
+;	__declspec(align(16)) Xmm a{5,20,30,1,20,-25,-35, -20};
+;	__declspec(align(16)) Xmm b { 1, 2, 5, 30, -12, -22, -30, 20 };
+;
+;	__declspec(align(16)) Xmm c{};
+;
+;
+;	SsePackedInt_Average(&a, &b, &c);
+;
+;	for (int i = 0; i < 8; i++) {
+;		printf("Result %i = %d \n",i, c.i16[i]);
+;	}
+;}
+
