5332071f8e5072de14c38904913e933b2f9adb82
Khalid <k.dahir243@hotmail.com>
2022-08-16 18:03:15 -0500

    Update mixed_mul_divCXX.cpp

--- Studies/cxx_asm/mixed_mul_divCXX.cpp
+++ Studies/cxx_asm/mixed_mul_divCXX.cpp
@@ -6,7 +6,7 @@
 
 
 // todo write these in assembly
-// (a + b + c) * (d + e + f)
+// (a* b* c) + (d * e * f)
 extern "C" int64_t signed_mul(int8_t a, int16_t b, int32_t c, int16_t d, int64_t e, int8_t f);
 // (a + b + c) / (d + e + f)
 extern "C" int unsigned_div(uint8_t a, uint16_t b, uint32_t c, uint16_t d, uint64_t e, uint8_t f, uint64_t * quo, uint64_t * rem);
