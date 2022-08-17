948d842a8afe5441280202580f31f99be3627508
Khalid <k.dahir243@hotmail.com>
2022-08-17 05:57:30 -0500

    Update min_max.asm

--- Studies/cxx_asm/min_max.asm
+++ Studies/cxx_asm/min_max.asm
@@ -9,12 +9,12 @@
 
 signed_min proc
 entry:
-	mov EAX, EDX 
-	cmp ECX, EDX
-	cmovl EAX, ECX
-	cmp EAX, R8D
-	cmovg EAX, R8D
-	movsxd RAX, EAX
+	mov EAX, EDX ; EAX = b
+	cmp ECX, EDX ; compare a with b
+	cmovl EAX, ECX ; EAX = (a < b) ? a : b
+	cmp EAX, R8D ; compare a or b with c
+	cmovg EAX, R8D ; EAX = (a:b > c) ? c : a:b
+	movsxd RAX, EAX ; signed extension double move RAX, EAX, since it maybe negative.
 
 	ret
 signed_min endp
@@ -22,9 +22,9 @@ signed_min endp
 
 unsigned_max proc
 entry:
-	mov EAX, EDX
-	cmp ECX, EDX
-	cmova EAX, ECX
+	mov EAX, EDX ; EAX = b
+	cmp ECX, EDX ; compare a with b
+	cmova EAX, ECX ; EAX = (b > a) ? b : a
 
 	ret
 unsigned_max endp
